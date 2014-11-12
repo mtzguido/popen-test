53df3218495ccd033c0821539681eccca75ce0cf
Guido Martínez <guido@vanguardiasur.com.ar>
2014-11-12 15:18:56 -0300

    add stderr to the example
    Signed-off-by: Guido Martínez <guido@vanguardiasur.com.ar>

--- popen-test.c
+++ popen-test.c
@@ -6,7 +6,13 @@ int main()
 	FILE *f;
 	char buf[1024];
 
-	f = popen("./test.sh", "r");
+	/*
+	 * The "2>/dev/null" is there so messages output
+	 * to stderr don't end up on _our_ stderr (since
+	 * we fork() inside popen(), they're the same
+	 * file).
+	 */
+	f = popen("./test.sh 2>/dev/null", "r");
 	if (!f) {
 		fprintf(stderr, "popen failed\n");
 		return 1;
--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+echo "Verbose message to stderr" >&2
+echo "These will be ignored" >&2
+
 for i in $(seq 1 10); do
 	echo "Tick $i"
 	sleep 1
